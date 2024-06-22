a8cde186f6e0b74caa45d4ec8221950c1e223eb2
PSadoyan <pavelsadoyan@gmail.com>
2024-06-22 15:13:18 -0400

    updated level

--- Map/Scripts/omnilight_3d_script.gd
+++ Map/Scripts/omnilight_3d_script.gd
@@ -3,9 +3,12 @@ extends DirectionalLight3D
 var myVec = Vector3(0,0,0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	OmniLight3D.light_energy = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("clap"):
+		OmniLight3D.light_energy = 5
+		await(5)
+		OmniLight3D.light_energy = 0
