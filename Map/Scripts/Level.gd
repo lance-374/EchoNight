50ece137bf9481d0c2f1f5fd8e210a1dca0e5b70
TerezoPena <62859453+TerezoPena@users.noreply.github.com>
2024-06-23 19:51:47 -0500

    MultiUpdate
    Added multiplayer features to menu allowing for character selection
    
    Know issues: the second player is currently invisible

--- Map/Scripts/Level.gd
+++ Map/Scripts/Level.gd
@@ -6,13 +6,27 @@ extends Node
 #@onready var health_bar = $CanvasLayer/HUD/HealthBar
 
 
-const Player = preload("res://PlayerControlledChars/Human/Scene/Human.tscn")
+const Human = preload("res://PlayerControlledChars/Human/Scene/Human.tscn")
+const Zombie = preload("res://PlayerControlledChars/Zombie/Scene/Zombie.tscn")
 const PORT = 3000
 var enet_peer = ENetMultiplayerPeer.new()
-
+var latestPlayerType
+var hasHost = false
 func _ready():
 	main_menu.connect("addressEntered",  joinAdressEntered)
 	main_menu.connect("hostWorldStart", startHost )
+	main_menu.connect("characterType", playerType)
+
+
+
+
+func playerType(type):
+	print(type)
+	latestPlayerType = type
+	if(!hasHost):
+		add_player(multiplayer.get_unique_id())
+		hasHost = true
+
 
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("escape"):
@@ -21,13 +35,11 @@ func _unhandled_input(_event):
 func startHost():
 	main_menu.hide()
 	#hud.show()
-	
 	enet_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = enet_peer
-	multiplayer.peer_connected.connect(add_player)
 	multiplayer.peer_disconnected.connect(remove_player)
 	
-	add_player(multiplayer.get_unique_id())
+	
 	
 	#upnp_setup()
 
@@ -35,13 +47,22 @@ func joinAdressEntered(address):
 	main_menu.hide()
 	#hud.show()
 	
+	
 	enet_peer.create_client(address, PORT)
 	multiplayer.multiplayer_peer = enet_peer
 
+
 func add_player(peer_id):
-	var player = Player.instantiate()
+	var player
+	if (latestPlayerType == ("Zombie")):
+		player= Zombie.instantiate()
+		print("Player Zombie")
+	elif(latestPlayerType == ("Human")):
+		player = Human.instantiate()
+		print("Player Human")
 	player.name = str(peer_id)
 	add_child(player)
+	
 	#if player.is_multiplayer_authority():
 		#player.health_changed.connect(update_health_bar)
 
--- Menu/Scripts/MainMenu.gd
+++ Menu/Scripts/MainMenu.gd
@@ -2,25 +2,55 @@ extends MenuBar
 
 signal addressEntered(address)
 signal hostWorldStart()
+signal characterType(type)
+var host = true
+@onready var textBoxForCharSelection = $"MarginContainer/VBoxContainer/CharacterSelection/Display Currently Playing"
+var type
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AddressEntry.hide()# Replace with function body.
+	$MarginContainer/VBoxContainer/CharacterSelection.hide()
+	textBoxForCharSelection.add_text("Please choose to play as a human or zombie")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _on_join_button_pressed():
-	$MarginContainer/VBoxContainer.hide()
-	$MarginContainer/AddressEntry.show() 
-	
+	host = false
+	$MarginContainer/VBoxContainer/MainMenu.hide()
+	$MarginContainer/VBoxContainer/CharacterSelection.show()
+
 
 
 func _on_address_entry_text_submitted(address):
-	emit_signal("addressEntered",address) # Replace with function body.
+	emit_signal("addressEntered",address)
+	emit_signal("characterType", type)
+
+
+
+
+func _on_confirm_pressed():
+	if host:
+		emit_signal("hostWorldStart")
+		emit_signal("characterType", type)
+	else: 
+		$MarginContainer/VBoxContainer/CharacterSelection.hide()
+		$AddressEntry.show()
+		
+	
+
+func _on_choose_zombie_pressed():
+	type = "Zombie" # Replace with function body.
+	textBoxForCharSelection.clear()
+	textBoxForCharSelection.add_text("You are currently playing as a : " + type)
+
+
+func _on_choose_human_pressed():
+	type = "Human" 
+	textBoxForCharSelection.clear()
+	textBoxForCharSelection.add_text("You are currently playing as a : " + type)
 
 
 func _on_host_button_pressed():
-	emit_signal("hostWorldStart") # Replace with function body.
+	$MarginContainer/VBoxContainer/MainMenu.hide()
+	$MarginContainer/VBoxContainer/CharacterSelection.show()
