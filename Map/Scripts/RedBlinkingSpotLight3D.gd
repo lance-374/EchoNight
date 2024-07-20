41858f9c4ae122046a7709f9e533be71943b92ac
Suyash Chitrakar <schitrakar@knox.edu>
2024-07-20 00:49:57 -0500

    Added lighting scripts and lights

--- Map/Scripts/RedBlinkingSpotLight3D.gd
+++ Map/Scripts/RedBlinkingSpotLight3D.gd
@@ -1,22 +1,16 @@
 extends SpotLight3D
 
-# Minimum and maximum duration in seconds for the light to stay in its current state
-var min_duration: float = 0.1
-var max_duration: float = 1.0
-#var visible = 
-# Function to toggle the visibility of the light
-func toggle_light():
-	visible = !visible
+# Parameters for flickering effect
+var min_intensity: float = 0
+var max_intensity: float = 3
+var flicker_speed: float = 1
 
-# Function to start the blinking effect
-func start_blinking():
-	toggle_light()
-	# Schedule the next toggle
-	var next_toggle_duration = randf_range(min_duration, max_duration)
-	await(get_tree().create_timer(next_toggle_duration))
-	start_blinking()
-
-# Called when the node enters the scene tree for the first time
-func _ready():
-	randomize() # Initialize random number generator
-	start_blinking()
+func _process(delta: float) -> void:
+	# Flicker intensity
+	light_energy = randf_range(min_intensity, max_intensity)
+	
+	# Optionally, flicker color for a more dynamic effect
+	#var r = randf_range(0.8, 1.0)  # Red component
+	#var g = randf_range(0.8, 1.0)  # Green component
+	#var b = randf_range(0.8, 1.0)  # Blue component
+	#light_color = Color(r, g, b, 1)
